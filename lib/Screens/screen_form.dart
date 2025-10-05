05b6d1b33c9fb8e27e17636ab67b2b1235f4295b
Morg-2025 <morganbwambale7@gmail.com>
2025-10-05 22:50:53 +0400

    4 changed

--- lib/Screens/screen_form.dart
+++ lib/Screens/screen_form.dart
@@ -1,20 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
-class ScreenForm extends StatelessWidget {
+class ScreenForm extends StatefulWidget {
+  @override
+  State<ScreenForm> createState() => _ScreenFormState();
+}
+
+class _ScreenFormState extends State<ScreenForm> {
+  
+  int age = 27;
+
+  final _formKey = GlobalKey<FormBuilderState>();
+
+  my_age_increase() {
+    age++;
+    print("Age now is ${age} years");
+
+    setState((){
+    });
+
+  }
+  my_age_decrease(){
+    age--;
+    setState(() {
+
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      title:Text("Form"),
-      ),
-      body: Padding(
-          padding:EdgeInsets.only(top:16.0, left:10),
-      child: Text("My Form"),
-    ),
-      );
+      appBar: AppBar(title: Text("Form")),
+      body: SingleChildScrollView(
+     child:  Column(
+        children: [
+          FormBuilder(
+              key: _formKey,
+       child: Column(
+              children:[
+                FormBuilderTextField(name: "First name"),
+              ],
+          ),
 
+         /* InkWell(
+            onTap: ()=>{
+              my_age_increase()
+            },
+
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "INCREASE",
+                style: TextStyle(color: Colors.blue, fontSize: 20),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text("I am ${age} years old"),
+          ),
+          InkWell(
+            onTap: ()=>{
+              my_age_decrease()
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "DECREASE",
+                style: TextStyle(color: Colors.blue, fontSize: 20),
+              ),
+            ),
+          ),*/
+      ),
+        ],
+      ),
+      ),
+    );
   }
 }
-
-
